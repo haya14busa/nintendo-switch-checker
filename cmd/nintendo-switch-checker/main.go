eb88ba8e5a0ec0a97e9f5d9dceba4ffc240293d9
haya14busa <haya14busa@gmail.com>
2017-06-17 07:21:04 +0000

    add nojima

--- cmd/nintendo-switch-checker/main.go
+++ cmd/nintendo-switch-checker/main.go
@@ -18,6 +18,10 @@ var (
 	once     = flag.Bool("once", false, "Check once")
 )
 
+const (
+	debugNotify = true
+)
+
 var sources = []nschecker.Source{
 	{
 		Name:          "Amazon - Nintendo Switch Joy-Con (L) / (R) グレー",
@@ -73,6 +77,16 @@ var sources = []nschecker.Source{
 		Name:        "omni7(iyec) - Nintendo Switch Joy-Con (L) ネオンブルー/ (R) ネオンレッド",
 		URL:         "http://iyec.omni7.jp/detail/4902370535716",
 		SoldOutText: `<input class="linkBtn js-pressTwice" type="submit" value="在庫切れ" title="在庫切れ"`,
+	},
+	{
+		Name:        "nojima - Nintendo Switch Joy-Con (L) / (R) グレー",
+		URL:         "https://online.nojima.co.jp/Nintendo-HAC-S-KAAAA-%E3%80%90NSW%E3%80%91-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E6%9C%AC%E4%BD%93-Joy-Con%28L%29-%28R%29-%E3%82%B0%E3%83%AC%E3%83%BC/4902370535709/1/cd/",
+		SoldOutText: `<span>完売御礼</span>`,
+	},
+	{
+		Name:        "nojima - Nintendo Switch Joy-Con (L) ネオンブルー/ (R) ネオンレッド",
+		URL:         "https://online.nojima.co.jp/Nintendo-HAC-S-KABAA-%E3%80%90NSW%E3%80%91-%E3%83%8B%E3%83%B3%E3%83%86%E3%83%B3%E3%83%89%E3%83%BC%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E6%9C%AC%E4%BD%93-Joy-Con%28L%29-%E3%83%8D%E3%82%AA%E3%83%B3%E3%83%96%E3%83%AB%E3%83%BC-%28R%29-%E3%83%8D%E3%82%AA%E3%83%B3%E3%83%AC%E3%83%83%E3%83%89/4902370535716/1/cd/",
+		SoldOutText: `<span>完売御礼</span>`,
 	},
 }
 
@@ -187,6 +201,9 @@ func (n *Notifier) Notify(state nschecker.State, s nschecker.Source) error {
 	}
 	msg := fmt.Sprintf("%v: %v (%v)", state, s.URL, s.Name)
 	params := slack.PostMessageParameters{EscapeText: false}
+	if !debugNotify {
+		return nil
+	}
 	_, _, err := n.Cli.PostMessage(n.channel, msg, params)
 	return err
 }
